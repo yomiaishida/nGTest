fe95b44a8f39b69a8e3f83b3ffa89d2c07ff5c1c
Abayomi Aishida <yomicorperate@gmail.com>
2021-11-09 21:54:00 +0100

    Shallow integration test and mockin an injected service

---
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -0,0 +1,39 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { of } from "rxjs";
+import { HeroService } from "../hero.service";
+import { HeroesComponent } from "./heroes.component"
+
+describe('HeroesComponent (shallow tests)', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
+
+    
+
+  beforeEach(() =>{
+    HEROES = [
+      {id:1, name: 'SpiderDude', strength: 8},
+      {id:2, name: 'Wonderful Woman', strength: 24},
+      {id:3, name: 'SuperDude', strength: 55}
+    ]
+    mockHeroService = jasmine.createSpyObj(['getHeroes, addHero, deleteHero']);
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroesComponent,
+      ],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService}
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+    fixture = TestBed.createComponent(HeroesComponent)
+  })
+
+  it('should set heroes correctly from the service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES))
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.heroes.length).toBe(3)
+  })
+})
