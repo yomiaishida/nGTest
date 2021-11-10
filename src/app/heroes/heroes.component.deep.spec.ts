697da7ebe9c0bfeae93567c6515453abffd040ec
Abayomi Aishida <yomicorperate@gmail.com>
2021-11-10 14:46:34 +0100

    Emitting events from children

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -55,7 +55,8 @@ describe('HeroesComponent (deep tests)', () => {
     fixture.detectChanges();
 
     const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
-    heroComponents[0].query(By.css('button')).triggerEventHandler('click', {stopPropagation: () => {}});
+    // (<HeroComponent>heroComponents[0].componentInstance).delete.emit(undefined);
+    heroComponents[0].triggerEventHandler('delete', null);
 
     expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0])
   })
