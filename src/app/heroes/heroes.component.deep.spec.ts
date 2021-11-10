43e9292198033267e0ee1277d6a272dd1ddb5c27
Abayomi Aishida <yomicorperate@gmail.com>
2021-11-10 14:33:54 +0100

    Triggering events on elements

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -27,7 +27,8 @@ describe('HeroesComponent (deep tests)', () => {
       ],
       providers: [
         { provide: HeroService, useValue :mockHeroService}
-      ]
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     fixture = TestBed.createComponent(HeroesComponent);
        
@@ -45,5 +46,17 @@ describe('HeroesComponent (deep tests)', () => {
       expect(heroComponentsDEs[i].componentInstance.hero).toEqual(HEROES[i])
 
     }
+  })
+  it(`should call heroService.delteHero when the Hero Component's 
+  delete button is clicked`, () => {
+    spyOn(fixture.componentInstance, 'delete');
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    fixture.detectChanges();
+
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    heroComponents[0].query(By.css('button')).triggerEventHandler('click', {stopPropagation: () => {}});
+
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0])
   })
 });
